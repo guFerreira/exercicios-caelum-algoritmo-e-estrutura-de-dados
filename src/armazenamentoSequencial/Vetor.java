3ec1315328a36f8de1772e9fb73ad4c999d0c823
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-10 10:40:49 -0300

    Adicionar implementaão método tamanho

--- src/armazenamentoSequencial/Vetor.java
+++ src/armazenamentoSequencial/Vetor.java
@@ -2,20 +2,25 @@ package armazenamentoSequencial;
 
 public class Vetor implements IVetor{
     private Aluno[] alunos = new Aluno[100];
+    private int totalDeAlunos = 0;
 
     @Override
     public void adiciona(Aluno aluno) {
-        for(int i = 0; i < alunos.length; i++){
+        //mais eficiente que a opção comentada, pois não precisa percorrer toda a lista
+        //para saber onde inserir
+        this.alunos[this.totalDeAlunos] = aluno;
+        this.totalDeAlunos++;
+        /*for(int i = 0; i < alunos.length; i++){
             if(alunos[i] == null){
                 alunos[i] = aluno;
             }
-        }
+        }*/
 
     }
 
     @Override
     public void adiciona(int posicao, Aluno aluno) {
-        
+
     }
 
     @Override
@@ -35,6 +40,22 @@ public class Vetor implements IVetor{
 
     @Override
     public int tamanho() {
-        return 0;
+        return this.totalDeAlunos;
+    }
+
+
+    public String toString() {
+        if (this.totalDeAlunos == 0) {
+            return "[]";
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for (int i = 0; i < this.totalDeAlunos - 1; i++) {
+            builder.append(this.alunos[i]);
+            builder.append(", ");
+        }
+        builder.append(this.alunos[this.totalDeAlunos - 1]);
+        builder.append("]");
+        return builder.toString();
     }
 }
