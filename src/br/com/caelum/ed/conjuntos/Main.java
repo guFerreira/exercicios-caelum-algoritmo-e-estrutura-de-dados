e9922f7b8a006b93662cec261bc74fa972203dc5
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-14 15:20:00 -0300

    Movendo testes da apostila para métodos

--- src/br/com/caelum/ed/conjuntos/Main.java
+++ src/br/com/caelum/ed/conjuntos/Main.java
@@ -1,19 +1,12 @@
 package br.com.caelum.ed.conjuntos;
 
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        MySet<String> m = new MySet();
-        m.add("Alegrete");
-        m.add("Amapá");
-        m.add("Brasilia");
-        System.out.println(m.getAll().toString());
-        System.out.println(m.contains("Alegrete"));
-        System.out.println(m.size());
-        m.remove("Alegrete");
-        System.out.println(m.contains("Alegrete"));
-        System.out.println(m.getAll().toString());
-        System.out.println(m.size());
 
+        testarFuncionalidadesSet();
+        testarConjuntoGenerico();
         /*MySet conjunto = new MySet();
         for (int i = 0; i < 100; i++) {
             conjunto.add("" + i);
@@ -35,5 +28,35 @@ public class Main {
         */
         // -2405 tempo marcado sem as modificações de redimencionar
         // -235 tempo marcado com as modificações de redimencionar
+    }
+
+    public static void testarFuncionalidadesSet(){
+        MySet<String> m = new MySet();
+        m.add("Alegrete");
+        m.add("Amapá");
+        m.add("Brasilia");
+        System.out.println(m.getAll().toString());
+        System.out.println(m.contains("Alegrete"));
+        System.out.println(m.size());
+        m.remove("Alegrete");
+        System.out.println(m.contains("Alegrete"));
+        System.out.println(m.getAll().toString());
+        System.out.println(m.size());
+    }
+    public static void testarConjuntoGenerico(){
+        MySet conjunto = new MySet();
+        conjunto.add("Rafael");
+        conjunto.add("Rafael");
+        conjunto.add("Ana");
+        conjunto.add("Paulo");
+        System.out.println(conjunto.getAll());
+
+       HashSet conjuntoHashSet = new HashSet();
+        conjuntoHashSet.add("Rafael");
+        conjuntoHashSet.add("Rafael");
+        conjuntoHashSet.add("Ana");
+        conjuntoHashSet.add("Paulo");
+        System.out.println(conjuntoHashSet);
+
     }
 }
