077afeb7cf0524a20616b777126feb8f3a8daac4
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-14 15:22:47 -0300

    10.3 - Exercícios: Armazenamento Associativo

---
+++ src/br/com/caelum/ed/Carro.java
@@ -0,0 +1,17 @@
+package br.com.caelum.ed;
+
+public class Carro {
+    private String nome;
+
+    public Carro(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
